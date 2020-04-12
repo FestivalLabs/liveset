bdd5dcbe25bd2189175c3575063174f6fa5e40e9
Val <valmack@protonmail.com>
2020-04-11 23:49:10 -0400

    Fix - Do not ignore file capitalization

---
+++ src/MainVideo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './MainVideo.css';
+import VideoContainer from './VideoContainer';
+
+function MainVideo() {
+  return (
+    <div className="mainVideo">
+     <VideoContainer />
+    </div>
+  );
+}
+
+export default MainVideo;
